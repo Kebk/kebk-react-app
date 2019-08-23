8bc7e890632df6b6237178696e4014d74c3e8196
ConradZhangcong <heisenberg0519@outlook.com>
2019-08-23 12:46:31 +0800

    :sparkles:css and img, add some loaders

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
+import './Page1.css'
+import image from './images/avator.jpg'
 
 class Hello extends Component {
   render() {
-    return <div>page1</div>
+    return (
+      <div className="page-box">
+        page1
+        <img src={image} width="100" height="100"/>
+      </div>
+    )
   }
 }
 
